432a2bdb155c230b62ca225d81e455c94379538b
Raj Gupta <rgm5823@gmail.com>
2022-06-30 11:03:55 +0530

    Assignment

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import TaskList from './components/TaskList.js';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <>
+      <TaskList />
+    </>
+  );
+}
+
+export default App;
